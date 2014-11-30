06ff9d1ca9a617eb376b153207c46047df00d22d
jonathan <jopray@gmail.com>
2014-11-29 22:13:19 -0800

    few Character updates

--- lib/generators.py
+++ lib/generators.py
@@ -7,22 +7,28 @@ class Character:
 	last_names = ["Johnson", "Davis", "Miller", "Jackson", "Robinson", "Clark", "Allen", "Carter"]
 	ages = ["child", "young", "middle-aged", "old"]
 	genders = ["male", "female"]
-	traits_1 = ["loyal", "opportunist", "selfish", "deceiptful", "stubborn", "pragmatic"]
-	traits_2 = ["weak", "strong"]
-
-
+	traits_1 = ["loyal", "opportunist", "selfish", "deceiptful", "stubborn", "pragmatic", "resolute"] # loyalties
+	traits_2 = ["weak", "thin", "flabby", "fat", "strong"] # physical
+	traits_3 = ["silent", "mean", "kind", "boring", "charismatic", "talkative"] # persona
+	# appearance (hair, eyes, nose)
 
 	def generate():
 		character = Character()
 		character.name = choice(Character.first_names) + " " + choice(Character.last_names)
+		character.gender = choice(Character.genders)
 		character.attributes = []
 		character.attributes.append(choice(Character.ages))
 		character.attributes.append(choice(Character.traits_1))
 		character.attributes.append(choice(Character.traits_2))
+		character.attributes.append(choice(Character.traits_3))
 		return character
+
+	def print(self):
+		print(self.name + " is a " + ", ".join(self.attributes) + " " + self.gender)
 
 # test Character creation
 c = Character.generate()
+c.print()
 
 def generate_environment():
 	pass
