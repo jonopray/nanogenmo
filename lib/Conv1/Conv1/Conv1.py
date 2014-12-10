fc3a2bfe6e90aac7c13ee4b41c73d051fd396274
HarryBCDresden <deathfire-san@hotmail.com>
2014-12-09 17:45:47 -0800

    Wes Files

--- lib/Conv1/Conv1/Conv1.py
+++ lib/Conv1/Conv1/Conv1.py
@@ -1,6 +1,7 @@
 import re
 from random import randint
 import time
+import math
 
 class Character:
 
@@ -27,8 +28,15 @@ class Conversation:
     def addChar(self, new_char):
         self.char_list.append(new_char) #Adds the new character to the list
 
-    def remChar(self,leave_char):
+    def remChar(self, leave_char):
         self.char_list.remove(leave_char)
+
+    def startConv(self):
+        next_talking=randint(0,len(self.char_list)-1)
+        while(len(self.char_list)>1):
+            spoke=self.talk(next_talking)
+            while (next_talking==spoke):  
+                next_talking=randint(0,len(self.char_list)-1)
 
     def endConv(self):
         self.char_list=[]
@@ -39,13 +47,13 @@ class Conversation:
         possible_statements=[]
         for row in range(0,len(self.sentences)-1):         #A list of possible sentences
             line_info=self.sentences[row].split(" | ")
-            if line_info[0]==talking_values:    #How well does the sentence match the personality
+            if self.personalityFit(line_info[0],char_num):    #How well does the sentence match the personality
                 possible_statements.append(line_info[1][:-1])
         if (len(possible_statements)>0 and talking.extroversion>0):      #If we have well-matched statements
             which_statement=randint(0,len(possible_statements)-1)   #Pick one at random
             statement=possible_statements[which_statement]
             if statement.count("_")>0:
-                statement=self.fillML(char_num,statement)   #If the statement is a madlib, fill it.
+                statement=self.fillML(line_info[0],statement, char_num)   #If the statement is a madlib, fill it.
         else:   #Otherwise, leave the conversation
             statement="Goodbye"
             self.remChar(self.char_list[char_num])
@@ -53,14 +61,11 @@ class Conversation:
         talking.extroversion-=1
         time.sleep(1)
         next_talking=randint(0,len(self.char_list)-1)
-        if(len(self.char_list)>1):
-            while (next_talking==char_num):  
-                next_talking=randint(0,len(self.char_list)-1)
-            self.talk(next_talking)
-        else:
+        if(len(self.char_list)<2):
             print "End of Conversation"
+        return char_num
 
-    def fillML(self, char_num, madlib):
+    def fillML(self, line_info, madlib, char_num):
         talking=self.char_list[char_num]    #The character who is speaking
         talking_values=talking.personality  #Grab their personaility from their object
         number_blanks=madlib.count("_")/2   #Determine the number of blanks: _A_, _V_, etc.
@@ -71,19 +76,46 @@ class Conversation:
             possible_blanks.append([])
             curr_blank=blanks[i][1:-1]
             for row in range(0,len(self.words)):    #Searches words for valid insertions
-                line_info=self.words[row].split(" | ")
-                if line_info[0]==curr_blank:
+                word_info=self.words[row].split(" | ")
+                if self.personalityFitML(line_info, word_info, char_num):
                     possible_blanks[i].append(line_info[1][:-1])
             which_choice=randint(0,len(possible_blanks[i])-1)       #Selects insertions at random
             madlib=madlib.replace(blanks[i],possible_blanks[i][which_choice],1)
         return madlib
+
+    def personalityFit(self, statement_info, char_num, *obj_num):    #Evaluates the statement information (.txt info) against the character personality
+        personality_values=self.char_list[char_num].personality     #*obj_num are who/what they are talking to/about
+        for i in obj_num:
+            print i
+        match=0
+        for i in range(0, min(len(statement_info),personality_values)-1):
+            match+=abs(float(statement_info[i])**2-float(personality_values[i])**2)
+        if match<1:
+            return 1
+        else:
+            return 0
+
+    def personalityFitML(self, statement_info, word_info, char_num, *obj_num):
+        personality_values=self.char_list[char_num].personality
+        object_perspective=[]
+        #for i in obj_num:
+            #object_perspective.append(self.char_list[char_num].perspective[i]);
+        match=0
+        for i in range(0, min(len(statement_info),personality_values)-1):
+            match+=abs(float(statement_info[i])**2-float(personality_values[i])**2)
+        for i in range(0, min(len(word_info),personality_values)-1):
+            match+=abs(float(word_info[i])**2-float(personality_values[i])**2)
+        if match<1:
+            return 1
+        else:
+            return 0
 
 
 
 
 Steve=Character()
 Steve.first_name="Steve"
-Steve.extroversion=7
+Steve.extroversion=2
 
 Carl=Character()
 Carl.first_name="Carl"
@@ -91,14 +123,15 @@ Carl.extroversion=3
 
 Joe=Character();
 Joe.first_name="Joe"
-Joe.extroversion=8
+Joe.extroversion=2
 
 Conv1=Conversation();
 Conv1.addChar(Steve)
 Conv1.addChar(Carl)
 Conv1.addChar(Joe)
 
-Conv1.talk(0)
+Conv1.startConv()
+print "Done"
 
 
 
